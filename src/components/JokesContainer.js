c2d70a6be22cc721ad9ff4c3d2bff9151c75a37d
Jose Torreblanca <josewhitetower@gmail.com>
2019-08-21 20:33:18 +0200

    changed py

--- src/components/JokesContainer.js
+++ src/components/JokesContainer.js
@@ -92,7 +92,7 @@ export default class JokesContainer extends Component {
       </div>
     );
     return (
-      <div className="JokesContainer flex container mx-auto py-40 flex-col-reverse md:flex-row lg:flex-row px-2 h-screen">
+      <div className="JokesContainer flex container mx-auto py-20 flex-col-reverse md:flex-row lg:flex-row px-2 h-screen">
         <div className="bg-blue-200 h-auto lg:w-6/12 md:w-8/12 py-6 shadow-lg text-center w-full z-10 text-red-500">
           <h1 className="font-semibold lg:my-16 md:my-16 text-5xl">
             Dad's Jokes
